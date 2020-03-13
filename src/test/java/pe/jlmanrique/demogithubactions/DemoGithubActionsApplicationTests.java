d84b7c391bbfc95b2980c60fe0115e212ac6d73f
Jose Luis Manrique C <jmanrique@pucp.pe>
2020-03-12 23:43:22 -0500

    Primer commit

---
+++ src/test/java/pe/jlmanrique/demogithubactions/DemoGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.jlmanrique.demogithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
